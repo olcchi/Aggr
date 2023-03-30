f3d6abf901724a845f1f9d71e7e71d2459598694
EKar <Earthb0y@outlook.com>
2023-03-30 12:55:52 +0800

    feat:update footer content

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,7 @@
 <template>
   <footer select-none p-5 text-center flex justify-center bottom-5 text-dark-100 dark:text-gray-200>
     <p text-xs>
-      Design and Created by
-      Ekar
+      Made by ekar | <a href="https://github.com/Ekarmore/Aggr" target="_blank">Github</a>
     </p>
   </footer>
 </template>
